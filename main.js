e2a3108e7a2eaf1022aaf20722f89e8a2e09fcb9
Yar4ikGIT <linderok12@gmail.com>
2024-10-26 15:14:22 +0000

    complemented the header

--- main.js
+++ main.js
@@ -15,3 +15,36 @@ window.addEventListener('scroll', () => {
 
   lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
 });
+
+
+const textBlocks = document.querySelectorAll('.text-block');
+const linkBlocks = document.querySelectorAll('.link-block');
+let index = 0;
+
+function changeText() {
+    // Скрываем все текстовые блоки и ссылки
+    textBlocks.forEach(block => {
+        block.style.opacity = '0';
+    });
+    
+    linkBlocks.forEach(block => {
+        block.style.opacity = '0';
+    });
+
+    // Показываем текущий текстовый блок с плавным эффектом
+    setTimeout(() => {
+        textBlocks[index].style.opacity = '1';
+    }, 100); // Небольшая задержка для плавного появления
+
+    // Показываем текущий блок с ссылкой с плавным эффектом
+    setTimeout(() => {
+        linkBlocks[index].style.opacity = '1';
+    }, 100); // Небольшая задержка для плавного появления
+
+    // Увеличиваем индекс, чтобы показать следующий блок, или сбрасываем до 0
+    index = (index + 1) % textBlocks.length;
+}
+
+// Вызываем функцию сразу и потом каждые 5 секунд
+changeText();
+setInterval(changeText, 5000);
